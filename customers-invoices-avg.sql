c66ba725bac89fcfe8382b9cdb571413b53141f8
TomJegou <tomjegou10@gmail.com>
2023-09-30 11:54:50 +0200

    add alias

--- customers-invoices-avg.sql
+++ customers-invoices-avg.sql
@@ -1,4 +1,4 @@
-select customers.FirstName, customers.LastName, avg(invoices.Total)
+select customers.FirstName, customers.LastName, avg(invoices.Total) as InvoicesAverage
 from customers
 inner join invoices on customers.CustomerId = invoices.CustomerId
 group by customers.CustomerId
