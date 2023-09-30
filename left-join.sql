fa155479333bceaca4c4e2530dba12b6f78555f1
TomJegou <tomjegou10@gmail.com>
2023-09-30 18:28:15 +0200

    test

--- left-join.sql
+++ left-join.sql
@@ -1,4 +1,4 @@
 SELECT artists.Name, albums.Title AS AlbumTitle
 FROM artists
-LEFT JOIN albums ON artists.ArtistId = albums.ArtistId
+LEFT OUTER JOIN albums ON artists.ArtistId = albums.ArtistId
 WHERE AlbumTitle IS NOT NULL
