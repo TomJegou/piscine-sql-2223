9e6cf6681258c1ca649bd3257d9a7c5cb615c018
TomJegou <tomjegou10@gmail.com>
2023-09-30 18:18:53 +0200

    correct

--- join-customers-invoices.sql
+++ join-customers-invoices.sql
@@ -1,3 +1,4 @@
 SELECT customers.FirstName, customers.LastName, customers.Company, invoices.Total AS InvoiceTotalPrice
 FROM customers
 INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId
+WHERE customers.Company IS NOT NULL
