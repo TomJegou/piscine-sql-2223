3c058a5ffbb35a9d7ccad95eca32fd4e1093e75c
TomJegou <tomjegou10@gmail.com>
2023-10-01 23:48:28 +0200

    update

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -3,5 +3,5 @@ SELECT InvoiceId, Total,
 		WHEN Total<5 THEN 'Price lower than 5$'
 		WHEN Total<10 THEN 'Price lower than 10$'
 		WHEN Total>10 THEN 'Price greater than 10$'
-	END
+	END as 'CASE'
 FROM invoices
