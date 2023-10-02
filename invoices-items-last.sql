95402ef50e0f7195707f333a3cab85884c72105e
TomJegou <tomjegou10@gmail.com>
2023-10-02 08:43:51 +0200

    typo

--- invoices-items-last.sql
+++ invoices-items-last.sql
@@ -4,6 +4,6 @@ WHERE tracks.TrackId = (
 	SELECT invoice_items.TrackId
 	FROM invoice_items
 	INNER JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
-	ORDER BY invcoices.InvoiceDate DESC
+	ORDER BY invoices.InvoiceDate DESC
 	LIMIT 1
 )
