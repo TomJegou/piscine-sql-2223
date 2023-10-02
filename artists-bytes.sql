3a2195ddf5fc2e699ffd980946ae2b991a0f4ed5
TomJegou <tomjegou10@gmail.com>
2023-10-02 09:14:55 +0200

    update

---
+++ artists-bytes.sql
@@ -0,0 +1,5 @@
+SELECT artists.Name AS 'ArtistName',  albums.Title AS 'AlbumName', tracks.Name AS 'TrackName', ROUND(tracks.Bytes/1000000, 2) || ' MB' AS 'MegaBytes'
+FROM tracks
+INNER JOIN albums ON tracks.AlbumId = albums.AlbumId
+INNER JOIN artists ON albums.ArtistId = artists.ArtistId
+WHERE albums.Title = 'American Idiot'
