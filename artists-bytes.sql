eeb91a48cb59eeda5f2e9b5e51a30e53390276cc
TomJegou <tomjegou10@gmail.com>
2023-10-02 09:19:37 +0200

    correct round

--- artists-bytes.sql
+++ artists-bytes.sql
@@ -1,4 +1,4 @@
-SELECT artists.Name AS 'ArtistName',  albums.Title AS 'AlbumName', tracks.Name AS 'TrackName', ROUND(tracks.Bytes/1000000, 2) || ' MB' AS 'MegaBytes'
+SELECT artists.Name AS 'ArtistName',  albums.Title AS 'AlbumName', tracks.Name AS 'TrackName', ROUND(ROUND(tracks.Bytes/1000,2)/10)/100 || ' MB' AS 'MegaBytes'
 FROM tracks
 INNER JOIN albums ON tracks.AlbumId = albums.AlbumId
 INNER JOIN artists ON albums.ArtistId = artists.ArtistId
