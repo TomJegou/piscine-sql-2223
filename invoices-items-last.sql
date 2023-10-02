87ac7caea4171bf1d611a60e66f21bdf15d1fac6
TomJegou <tomjegou10@gmail.com>
2023-10-02 08:43:01 +0200

    update

--- invoices-items-last.sql
+++ invoices-items-last.sql
@@ -1,5 +1,9 @@
 SELECT tracks.Name
 FROM tracks
-INNER JOIN invoice_items ON tracks.TrackId = invoice_items.TrackId
-INNER JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
-ORDER BY invoices.InvoiceDate DESC
+WHERE tracks.TrackId = (
+	SELECT invoice_items.TrackId
+	FROM invoice_items
+	INNER JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
+	ORDER BY invcoices.InvoiceDate DESC
+	LIMIT 1
+)
