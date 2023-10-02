b64a0ed098bf085cc79a1a0ec71abe523e8d6b93
TomJegou <tomjegou10@gmail.com>
2023-10-02 09:42:24 +0200

    correct

--- artists-bytes.sql
+++ artists-bytes.sql
@@ -1,4 +1,4 @@
-SELECT artists.Name AS 'ArtistName',  albums.Title AS 'AlbumName', tracks.Name AS 'TrackName', ROUND(tracks.Bytes, 2) || ' MB' AS 'MegaBytes'
+SELECT artists.Name AS 'ArtistName',  albums.Title AS 'AlbumName', tracks.Name AS 'TrackName',  printf("%.2f MB", ROUND(ROUND(tracks.Bytes/1000)/10)/100) AS 'MegaBytes'
 FROM tracks
 INNER JOIN albums ON tracks.AlbumId = albums.AlbumId
 INNER JOIN artists ON albums.ArtistId = artists.ArtistId
